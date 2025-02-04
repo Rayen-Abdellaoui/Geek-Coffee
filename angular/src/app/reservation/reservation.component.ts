352b203378c5b3a9b9fd59ed8c5824c67db72224
AyoubGharbi27 <ayoubgharbi932@gmail.com>
2025-02-04 15:40:03 +0100

    reservation_done

--- angular/src/app/reservation/reservation.component.ts
+++ angular/src/app/reservation/reservation.component.ts
@@ -1,11 +1,37 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
+  imports: [CommonModule],
   selector: 'app-reservation',
-  imports: [],
   templateUrl: './reservation.component.html',
-  styleUrl: './reservation.component.css'
+  styleUrls: ['./reservation.component.css']
 })
 export class ReservationComponent {
-
+  backgroundImage = 'assets/resback3.jpg';
+  cards = [
+    {
+      image: "assets/offer1.jpg",
+      title: 'Playstation',
+      text: 'enjoy your favorite playstation games.'
+    },
+    {
+      image: "assets/gallery10.jpg",
+      title: 'arcade',
+      text: 'participate in retro arcade game tournaments.'
+    },
+    {
+      image: "assets/offer2.jpg",
+      title: 'Eat your fill',
+      text: 'enjoy a variety of healthy and delicious meals.'
+    },
+    {
+      image: "assets/offer3.jpg",
+      title: 'special offers',
+      text: 'keep an eye on our irresistable deals.'
+    }
+  ];
+  ngOnInit() {
+    console.log(this.cards); 
+  }
 }
